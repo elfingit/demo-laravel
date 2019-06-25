3851698afffff10e1bb07f308e661fbf5b14792c
Andrey Sergeev <andreylong@gmail.com>
2019-06-25 10:11:27 +0300

    new UI for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,55 @@
-@extends('layouts.app')
+@extends('layouts.crm')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+    <div class="demo-layout mdl-layout mdl-js-layout login-page" id="app">
+        <main class="mdl-layout__content">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-shadow--2dp login-container">
+                    <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">{{ __('Login') }}</h2>
+                    </div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                    <div class="mdl-card__supporting-text">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input @error('email') is-invalid @enderror" type="email" id="email">
+                                <label class="mdl-textfield__label" for="email">{{ __('E-Mail Address') }}</label>
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="mdl-textfield__error">{{ $message }}</span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="mdl-textfield mdl-js-textfield">
+                                <input class="mdl-textfield__input @error('password') is-invalid @enderror" type="password" id="password">
+                                <label class="mdl-textfield__label" for="password">{{ __('Password') }}</label>
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="mdl-textfield__error">{{ $message }}</span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                            <div class="mdl-textfield mdl-js-textfield">
+                                <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="remember">
+                                    <input type="checkbox" class="mdl-checkbox__input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <span class="mdl-checkbox__label">{{ __('Remember Me') }}</span>
+                                </label>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                    </div>
+                    <div class="mdl-card__actions mdl-card--border">
+                        <div class="mdl-grid">
+                            <div class="mdl-cell">
+                                <button type="submit" class="mdl-button mdl-js-button">{{ __('Login') }}</button>
+                            </div>
+                            @if (Route::has('password.request'))
+                                <div class="mdl-layout-spacer"></div>
+                                <div class="mdl-cell">
+                                    <a class="mdl-button mdl-js-button" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
-                            </div>
+                                </div>
+                            @endif
                         </div>
+                    </div>
                     </form>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/crm.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-light_blue.min.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
