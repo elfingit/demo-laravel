7973debe53639776975a428af535569d41d0a6cf
Andrey Sergeev <andreylong@gmail.com>
2019-06-25 16:58:27 +0300

    brand migration

--- database/migrations/2019_06_25_131008_create_brands_table.php
+++ database/migrations/2019_06_25_131008_create_brands_table.php
@@ -15,7 +15,36 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('api_code')->unique();
+            $table->string('logo');
+            $table->unsignedInteger('refresh_data_time');
+
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+
+            $table->unsignedMediumInteger('main_min');
+            $table->unsignedMediumInteger('main_max');
+            $table->unsignedMediumInteger('main_drawn');
+
+            $table->unsignedMediumInteger('bonus_min');
+            $table->unsignedMediumInteger('bonus_max');
+            $table->unsignedMediumInteger('bonus_drawn');
+
+			$table->enum('same_balls', ['Y', 'N']);
+
+	        $table->unsignedMediumInteger('digits');
+	        $table->unsignedMediumInteger('drawn');
+
+	        $table->string('option_desc')->nullable();
+
+	        $table->bigInteger('owner_id')->index();
+
             $table->timestamps();
+
+            $table->foreign('owner_id')
+	            ->references('id')
+	            ->on('users');
         });
     }
 
