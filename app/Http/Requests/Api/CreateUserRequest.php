b11fcbcd7a946222edec9ee992a84923ab82c7b7
Andrey Sergeev <andreylong@gmail.com>
2019-07-16 15:39:18 +0300

    fix

--- app/Http/Requests/Api/CreateUserRequest.php
+++ app/Http/Requests/Api/CreateUserRequest.php
@@ -24,6 +24,8 @@ class CreateUserRequest extends AbstractRequest
 	        'year_of_birth'     => 'required|numeric|min:'. (date('Y') - 100)
 	                               .'|max:'. (date('Y') - 18),
 	        'country'           => 'required|in:' . implode(',', config('countries')),
+	        'terms_and_conditions' => 'accepted',
+	        'privacy_policy'        => 'accepted',
 	        'host'          => 'required|regex:/(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)/',
 	        'favicon_url'   => 'required|url'
         ];
