65d173fa07960b786230a98902299861f1947fc6
Andrey Sergeev <andreylong@gmail.com>
2019-07-24 10:34:23 +0300

    fix

--- resources/views/brand/_result.blade.php
+++ resources/views/brand/_result.blade.php
@@ -5,7 +5,7 @@
 </ul>
 <span class="extra_ball">{{ $result->results->extra_ball }}</span>
 <div class="additional_game">
-    @if(isset($result->results->additional_games->megaplier))
+    @if(is_object($result->results->additional_games) && isset($result->results->additional_games->megaplier))
     <span class="label">{{ __('Megaplier') }}:<i>{{ $result->results->additional_games->megaplier }}</i></span>
     @endif
 </div>
