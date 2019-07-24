84eb8fb297a22788ca6ac1deb93296d16de9e558
Andrey Sergeev <andreylong@gmail.com>
2019-07-24 10:37:33 +0300

    fix

--- resources/views/brand/_result.blade.php
+++ resources/views/brand/_result.blade.php
@@ -3,7 +3,13 @@
     <li>{{ $ball }}</li>
     @endforeach
 </ul>
+@if(is_array($result->results->extra_ball))
+    @foreach($result->results->extra_ball as $ball)
+        <span class="extra_ball">{{ $ball }}</span>
+    @endforeach
+@else
 <span class="extra_ball">{{ $result->results->extra_ball }}</span>
+@endif
 <div class="additional_game">
     @if(is_object($result->results->additional_games) && isset($result->results->additional_games->megaplier))
     <span class="label">{{ __('Megaplier') }}:<i>{{ $result->results->additional_games->megaplier }}</i></span>
