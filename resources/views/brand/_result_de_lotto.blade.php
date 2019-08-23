698ebee5ebdfa9f53834d4bd636aacf4c0db0d73
Andrey Sergeev <andreylong@gmail.com>
2019-08-23 15:17:30 +0300

    fix [APPV1-117]

--- resources/views/brand/_result_de_lotto.blade.php
+++ resources/views/brand/_result_de_lotto.blade.php
@@ -30,5 +30,13 @@
                 <span class="mdl-list__item-text-body">{{ $result->results->additional_games->super6 }}</span>
             </span>
         </div>
+        <div class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+                <span><b>{{ __('Jackpot') }}:</b></span>
+            </span>
+            <span class="mdl-list__item-secondary-content">
+                <span class="mdl-list__item-text-body">{{ number_format($result->jack_pot,2) }}</span>
+            </span>
+        </div>
     </div>
 </div>
